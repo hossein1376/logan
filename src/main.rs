c9d7e639c45129af5e40e946fe3619a061b0bcc2
Hossein Yazdani <hosseinyazdani1376@gmail.com>
2023-10-10 16:28:24 +0330

    Read json file and parse it into a HashMap with time as its key

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+fn process_log_files(path: &str) -> Result<HashMap<String, HashMap<String, String>>, io::Error> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    let mut data: HashMap<String, HashMap<String, String>> = HashMap::new();
+
+    for line in reader.lines() {
+        let mut entry: HashMap<String, String> = serde_json::from_str(line?.as_str())?;
+
+        let time = entry.remove("time").unwrap();
+        data.insert(time, entry);
+    }
+
+    Ok(data)
+}
+
+
 fn main() {
-    println!("Hello, World!")
+    let res = process_log_files("./file.log").unwrap();
+    println!("{:#?}", res)
 }
